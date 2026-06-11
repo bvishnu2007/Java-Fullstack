eb3241e9304d1f7cf6e358aaf3988f0d76a65610
PavanRedbangle <pavan@redbangle.com>
2026-06-11 13:32:11 +0530

    JAVA-FULLSTACK

---
+++ HTML-CSS/script.js
@@ -0,0 +1,46 @@
+/*let name="hi";
+var age = 20;
+const PI = 3.14;
+console.log(name);
+console.log(age);
+console.log(PI);
+document.write(name);
+document.write(age+PI);
+age+=3;
+console.log(age);
+age-=2;
+console.log(age);
+age*=2;
+console.log(age);
+let x=10;
+console.log(x>age);*/
+/*let food = "pizza";
+let drink = "coke";
+if (food == drink) {
+    console.log("I like pizza");
+} else {
+    console.log("I like coke");
+}
+let amount = 4800;
+let notes500 = Math.floor(amount / 500);
+amount = amount % 500;
+let notes200 = Math.floor(amount / 200);
+amount = amount % 200;
+let notes100 = Math.floor(amount / 100);
+console.log("500 Notes:", notes500);
+console.log("200 Notes:", notes200);
+console.log("100 Notes:", notes100);
+console.log("withdrawal amount:", 4800);*/
+for (let i = 1; i <= 30; i++) { 
+    console.log("day " + i + " completed");
+}
+
+for (let i = 0; i <5; i ++) {
+    if (i %2==0) {
+        console.log("gifted");
+    }   
+    else {
+        console.log("not gifted");
+    }    
+}
+
