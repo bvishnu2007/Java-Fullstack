11093d85934fa96f311a0fefb63b5ca606123f89
PavanRedbangle <pavan@redbangle.com>
2026-06-12 14:26:17 +0530

    JAVA-FULLSTACK

---
+++ HTML-CSS/input.js
@@ -0,0 +1,15 @@
+function greet_afternoon() {
+            let text = document.getElementById("greet");
+            text.innerHTML = "Good afternoon";
+        }
+ function turn_on() { 
+            let light = document.getElementById("light");
+            light.src = "https://www.w3schools.com/js/pic_bulbon.gif";
+        }
+  function turn_off() {            let light = document.getElementById("light");
+            light.src = "https://www.w3schools.com/js/pic_bulboff.gif";
+        }            
+function change_color() {
+            let colour = document.getElementById("cap");
+            colour.style.backgroundColor = "red";
+        }
